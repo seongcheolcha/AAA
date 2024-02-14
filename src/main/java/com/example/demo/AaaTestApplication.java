905bce8880cd6c7a6fdeb1be2c85e81e02aef957
seongcheolcha <accell5249@gmail.com>
2024-02-14 11:35:46 +0900

    aaa

---
+++ src/main/java/com/example/demo/AaaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/aaa/aaaaa.java
@@ -0,0 +1,7 @@
+package com.example.demo.aaa;
+
+public class aaaaa {
+
+	
+	
+}
